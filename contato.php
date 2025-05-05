cca495c45cfbe49da35d46666f9115272f8462b2
VitorSanto-ux <vitor.h.santo@alunosenai.br>
2025-05-05 13:48:13 -0300

    css

---
+++ contato.php
@@ -0,0 +1,7 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+
+echo "O e-mail de '$nome' foi enviado com sucesso!";
+?>
---
+++ dados.php
@@ -0,0 +1,8 @@
+<?php
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$data_nasc = $_POST['data_nasc'];
+$email = $_POST['email'];
+
+echo "O participante '$nome' foi cadastrado com sucesso!";
+?>
